2d041f2c32011dd6a7a401425ccb30b469c7d0d3
Rishikesh Dongre <104708547+rishidongregitub@users.noreply.github.com>
2024-09-09 11:08:34 +0000

    Initial Setup with side bar component

---
+++ side_bar/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Side from './Components/Side';
+function App() {
+  return (
+    <>
+      <Side/>
+    </>
+  );
+}
+
+export default App;
---
+++ side_bar/src/Components/Side.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Side=()=>{
+    return (
+        <>
+            <h1></h1>
+        </>
+    )
+}
+export default Side;
