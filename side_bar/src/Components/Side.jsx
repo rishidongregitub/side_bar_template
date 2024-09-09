8f422ef2ecf5237619d3bb5f20557da0a897759f
Rishikesh Dongre <104708547+rishidongregitub@users.noreply.github.com>
2024-09-09 11:27:05 +0000

    install lucide-react library for icons

--- side_bar/src/Components/Side.jsx
+++ side_bar/src/Components/Side.jsx
@@ -1,9 +1,18 @@
 import React from "react";
-
+import {ChevronFirst} from 'lucide-react'
 const Side=()=>{
     return (
         <>
-            
+            <div className="h-screen">
+                <div className="h-full flex flex-col bg-white border-r shadow-">
+                    <div className="p-4 pb-2 flex justify-between items-center">
+                        <img src="http://img.logoipsum.com/243.svg" alt="" className="w-32" />
+                    </div>
+                    <button className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100">
+                        <ChevronFirst/>
+                    </button>
+                </div>
+            </div>
         </>
     )
 }
